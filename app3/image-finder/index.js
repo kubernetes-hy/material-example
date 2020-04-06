a04092af0393067d08280db7a79057eaab67692b
Jami Kousa <jami.kousa@helsinki.fi>
2020-04-06 15:32:04 +0300

    app3

---
+++ app3/image-finder/index.js
@@ -0,0 +1,40 @@
+const Koa = require('koa')
+const path = require('path')
+const fs = require('fs')
+const axios = require('axios')
+const app = new Koa()
+const PORT = process.env.NODE_ENV || 3001
+
+const directory = path.join('/', 'usr', 'src', 'app', 'files')
+const filePath = path.join(directory, 'image.jpg')
+
+const fileAlreadyExists = async () => new Promise(res => {
+  fs.stat(filePath, (err, stats) => {
+    if (err || !stats) return res(false)
+    return res(true)
+  })
+})
+
+const findAFile = async () => {
+  if (await fileAlreadyExists()) return
+
+  await new Promise(res => fs.mkdir(directory, (err) => res()))
+  const response = await axios.get('https://picsum.photos/200', { responseType: 'stream' })
+  response.data.pipe(fs.createWriteStream(filePath))
+}
+
+const removeFile = async () => new Promise(res => fs.unlink(filePath, (err) => res()))
+
+app.use(async ctx => {
+  if (ctx.path.includes('favicon.ico')) return
+  // Get a new image
+  await removeFile()
+  findAFile()
+  ctx.status = 200
+});
+
+console.log('Started')
+
+findAFile()
+
+app.listen(PORT)
---
+++ app3/image-response/index.js
@@ -0,0 +1,28 @@
+const Koa = require('koa')
+const path = require('path')
+const fs = require('fs')
+const app = new Koa()
+const PORT = process.env.NODE_ENV || 3000
+
+const directory = path.join('/', 'usr', 'src', 'app', 'files')
+const filePath = path.join(directory, 'image.jpg')
+
+const getFile = async () => new Promise(res => {
+  fs.readFile(filePath, (err, buffer) => {
+    if (err) return console.log('FAILED TO READ FILE', '----------------', err)
+    res(buffer)
+  })
+})
+
+app.use(async ctx => {
+  if (ctx.path.includes('favicon.ico')) return
+  // Get a new image
+  ctx.body = await getFile()
+  ctx.set('Content-disposition', 'attachment; filename=image.jpg');
+  ctx.set('Content-type', 'image/jpeg');
+  ctx.status = 200
+});
+
+console.log('Started')
+
+app.listen(PORT)
