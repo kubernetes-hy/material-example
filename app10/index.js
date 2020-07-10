9752d6c4bede0eb6c5eedf5b66c8d88b9b87b29d
Jami Kousa <jami.kousa@helsinki.fi>
2020-07-10 18:14:42 +0300

    app10 ongoing mayhem

--- app10/index.js
+++ app10/index.js
@@ -1,13 +1,22 @@
-const countdown = (time) => {
-  if (time < 0) time = 0
-  console.log('Time:', time)
+const k8s = require('@kubernetes/client-node')
+const mustache = require('mustache')
+const request = require('request')
+const JSONStream = require('json-stream')
+const fs = require('fs').promises
 
-  setTimeout(() => countdown(time - 1), 1200) // Dramatic timing
+let time = Number(process.argv[2]) || 10
+
+const countdown = () => {
+//  console.log('Time:', time)
+
+  if (time > 0) time--
+
+  setTimeout(() => countdown(), 1200) // Dramatic timing
 }
 
-countdown(Number(process.argv[2]) || 10)
+countdown()
 
-// The loop above prevents graceful exits
+// The loop prevents graceful exits
 
 const signals = {
   'SIGHUP': 1,
@@ -18,3 +27,62 @@ const signals = {
 Object.keys(signals).forEach(s => {
   process.on(s, () => process.exit(128 + signals[s]))
 })
+
+// Use Kubernetes client to interact with Kubernetes
+
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+
+const opts = {}
+kc.applyToRequest(opts)
+
+const client = kc.makeApiClient(k8s.CoreV1Api);
+
+const sendRequestToApi = async (api, method = 'get', options = undefined) => new Promise((resolve, reject) => request[method](`${kc.getCurrentCluster().server}${api}`, {...opts, ...options}, (err, res) => err ? reject(err) : resolve(JSON.parse(res.body))))
+
+const fieldsFromObject = (object) => ({
+  name: `${object.metadata.name}-countdown`,
+  deployment_name: `${object.metadata.name}-countdown-dep`,
+  namespace: object.metadata.namespace,
+  image: object.spec.image,
+  length: object.spec.length
+})
+
+const getDeploymentYAML = async (object) => {
+  const deploymentTemplate = await fs.readFile("deployment.mustache", "utf-8")
+  const fields = fieldsFromObject(object)
+  return mustache.render(deploymentTemplate, fields)
+}
+
+const createDeployment = async (object) => {
+  const yaml = await getDeploymentYAML(object)
+  const { namespace } = fieldsFromObject(object)
+  sendRequestToApi(`/apis/apps/v1/namespaces/${namespace}/deployments`, 'post', {
+    headers: {
+      'Content-Type': 'application/yaml'
+    },
+    body: yaml
+  })
+}
+
+const removeDeployment = async (object) => {
+  const { namespace, deployment_name } = fieldsFromObject(object)
+  sendRequestToApi(`/apis/apps/v1/namespaces/${namespace}/deployments/${deployment_name}`, 'delete')
+}
+
+const maintainStatus = async () => {
+  (await client.listNode()).body // A bug in the client(?) was fixed by sending a request and not caring about response
+
+  const stream = new JSONStream()
+  stream.on('data', async ({ type, object }) => {
+    if (type === 'ADDED') {
+      createDeployment(object)
+    }
+    if (type === 'DELETED') {
+      removeDeployment(object)
+    }
+  })
+  request.get(`${kc.getCurrentCluster().server}/apis/stable.dwk/v1/countdowns?watch=true`, opts).pipe(stream)
+}
+
+maintainStatus().catch(err => console.log('WHAT', err))
