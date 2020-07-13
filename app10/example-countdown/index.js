a4b016d930a2f8bff670eb7b44aded4a9c96becb
Jami Kousa <jami.kousa@helsinki.fi>
2020-07-13 14:08:38 +0300

    countdown simplified

--- app10/example-countdown/index.js
+++ app10/example-countdown/index.js
@@ -1,11 +1,13 @@
 let time = Number(process.argv[2]) || 10
 
 const countdown = () => {
+  if (time < 3) console.log('Time is coming to an end!')
+
   console.log('Time:', time)
 
-  if (time > 0) time--
+//  if (time > 0) time--
 
-  setTimeout(() => countdown(), 1200) // Dramatic timing
+//  setTimeout(() => countdown(), 1200) // Dramatic timing
 }
 
 countdown()
